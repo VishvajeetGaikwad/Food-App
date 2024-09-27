51e065689682b8d5159a6ccce91dffa81aac54fa
VishvajeetGaikwad <gaikwadvishvajeet2@gmail.com>
2024-09-27 15:35:33 +0530

    Restaurant detail page has been added

---
+++ client/src/components/RestaurantDetail.tsx
@@ -0,0 +1,40 @@
+import { Badge } from "./ui/badge";
+import { Timer } from "lucide-react";
+
+const RestaurantDetail = () => {
+  return (
+    <div className="max-w-6xl mx-auto my-10">
+      <div className="w-full">
+        <div className="relative w-full h-32 md:h-64 lg:h-72">
+          <img
+            src="https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt="res_image"
+            className="object-cover w-full h-full rounded-lg shadow-lg"
+          />
+        </div>
+        <div className="flex flex-col md:flex-row justify-between">
+          <div className="my-5">
+            <h1 className="font-medium text-xl">Tandoori Tadakha</h1>
+            <div className="flex gap-2 my-3">
+              {["Biryani", "Momo"].map((cuisine: string, idx: number) => (
+                <Badge className="bg-black text-white" key={idx}>
+                  {cuisine}
+                </Badge>
+              ))}
+            </div>
+            <div className="flex md:flex-row flex-col gap-2 my-5">
+              <div className="flex items-center gap-2">
+                <Timer className="w-5 h-5" />
+                <h1 className="flex items-center gap-2 font-medium">
+                  Delivery Time: <span className="text-[#D19254]">35 mins</span>
+                </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantDetail;
