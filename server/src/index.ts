bf674ce085cbc3696dc931d895179d5897c57805
VishvajeetGaikwad <gaikwadvishvajeet2@gmail.com>
2024-10-13 10:21:43 +0530

    some changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173", // Allow localhost in development, production URL in production
   credentials: true,
 };
 app.use(cors(corsOptions));
